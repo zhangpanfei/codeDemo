31f8591d130716e8215896fba52b72449396244e
zhangpanfei <zhangpanfei@wafersystems.com>
2022-02-17 21:29:30 +0800

    fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,6 +2,17 @@
 import fs from 'fs'
 
 export default function handler(req, res) {
-  fs.writeFile('./public/test.txt', Date.now().toString(), () => { })
-  res.status(200).json({ name: 'John Doe123' })
+  fs.writeFile('./public/test.txt', Date.now().toString(), (err) => {
+    if(err) {
+      return res.status(200).json({ name: '出错了写文件' })
+    }
+    fs.readFile('./public/test.txt', { encoding: 'utf-8' }, (err, data) => {
+      if (err) {
+        return res.status(200).json({ name: '出错了' })
+      }
+      res.status(200).json({ name: data })
+    })
+  })
+  
+
 }
