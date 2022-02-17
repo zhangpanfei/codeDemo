2de59d2c17fa238db9b470882cb8144ec251ead0
zhangpanfei <zhangpanfei@wafersystems.com>
2022-02-17 21:20:46 +0800

    fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from 'fs'
 
 export default function handler(req, res) {
+  fs.writeFile('./public/test.txt', Date.now().toString(), () => { })
   res.status(200).json({ name: 'John Doe' })
 }
