f23a4a9518f71104982cfb6fdc24e3e45ed46fff
zhangpanfei <zhangpanfei@wafersystems.com>
2022-02-24 20:39:08 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,2 @@
-const withCss = require('@zeit/next-css')
-
-if(typeof require !== 'undefined'){
-  require.extensions['.css']=file=>{}
-}
-
-module.exports = withCss({})
+const withLess = require("next-with-less");
+module.exports = withLess({})
