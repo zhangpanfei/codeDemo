8981a4b33e06ae61957c15733b44f4a1827f9383
zpfei <zpfv@qq.com>
2022-04-09 13:20:38 +0800

    fix

--- pages/api/echo.js
+++ pages/api/echo.js
@@ -32,11 +32,11 @@ export default function handler(req, res) {
         const data = Array.from({ length: num }).map((v, k) => {
             return `${k + 1} xxxxx`
         })
-        res.json(success({ type: 'list', res: data }))
+        return res.json(success({ type: 'list', res: data }))
     }
 
     if (command.includes('image')) {
-        res.json(success({ type: 'image', res: 'https://joeschmoe.io/api/v1/7' }))
+        return res.json(success({ type: 'image', res: 'https://joeschmoe.io/api/v1/7' }))
     }
 
     res.send(error('Command is Error! Please enter a command containing the keyword "list" or "image" or "ping"'))
